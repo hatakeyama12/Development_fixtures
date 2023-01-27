426a714c8394ffde34829dd9b1fe926eb1b539de
tanifuji <h.tanifuji.sys22@morijyobi.ac.jp>
2023-01-27 12:06:24 +0900

    一覧表示機能を新規作成しました。

--- src/main/java/dao/Development_fixturesDAO.java
+++ src/main/java/dao/Development_fixturesDAO.java
@@ -5,7 +5,10 @@ import java.net.URISyntaxException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 import dto.Development_fixtures;
 
@@ -46,5 +49,34 @@ public class Development_fixturesDAO {
 			System.out.println(result + "件更新しました。");
 		}
 		return result;
+	}
+
+	public static List<Development_fixtures> Allgoods() {		
+		String sql = "SELECT * FROM Development_fixtures";		
+		List<Development_fixtures> goods = new ArrayList<>();
+		
+		try (
+				Connection con = getConnection();
+				PreparedStatement pstmt = con.prepareStatement(sql);
+				){
+			try (ResultSet rs = pstmt.executeQuery()){
+				while(rs.next()) {
+					int id = rs.getInt("id");
+					String name = rs.getString("name");
+					String content = rs.getString("content");
+					int quantity = rs.getInt("quantity");
+
+					Development_fixtures employee = new Development_fixtures(id, name, content, quantity);
+					
+					goods.add(employee);
+				}
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (URISyntaxException e1) {
+			e1.printStackTrace();
+		}
+
+		return goods;
 	}
 }
